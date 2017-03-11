cb20c7f2cecdaf888d478d4888c0ca171ffdfa25
Joris Hens <joris@goodbytes.be>
2017-03-11 18:58:44 +0100

    Added Axios and auth headers

--- source/index.js
+++ source/index.js
@@ -1,4 +1,10 @@
 import Push from 'push.js';
+import axios from 'axios/dist/axios';
+
+axios.defaults.headers.common = {
+    'X-Requested-With': 'XMLHttpRequest',
+    'Authorization' : 'Bearer ' + "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImQ3YzUxMTE5ZTlmNjQ3MjE2YjA3NjNjY2EwODFmZGQwZTU2YjlkNTExNDkwNTBkMThhZTEwNTE5ZTFmNjI4MGI1OGQ0ODQ2MjFiZDE0NTE3In0.eyJhdWQiOiIxIiwianRpIjoiZDdjNTExMTllOWY2NDcyMTZiMDc2M2NjYTA4MWZkZDBlNTZiOWQ1MTE0OTA1MGQxOGFlMTA1MTllMWY2MjgwYjU4ZDQ4NDYyMWJkMTQ1MTciLCJpYXQiOjE0ODkyNTQzMzAsIm5iZiI6MTQ4OTI1NDMzMCwiZXhwIjoxNTIwNzkwMzMwLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.puX4iUIeUIZEcH6dW9VVmNlE5mWeQSKiC_oCnCBiRWo-xGz5oDYtUZ4bk_pRpeKkogfL2eUiyZqHr2JVsq_HXNCDEULbABeC88WZJsQGlSs83eVbMsHszs2cSvK5QzLP-DN94OliKEyOA5u4Unolh69qR9SlKo-opCXc4ARjqZg2RRfV21T7OI4RQFrEOYfCDMflqVMy8f_kJovdxEA5oxmxQH7LLUnGcBOOB9H4w1XBeWwhm4OuI4pxzWCB5js6dz7mmc1cxpThrKayp_urq_XxnIJBUavY4jlOnM_UAsqRVSZQxXyAg_BTLMhR_ifOqjTwydqBwEQY64ZUgf1yULGTNMzf3G5R4Kj6ja6H0f0s0aJcvKC3Kb2WQ0fg_x7D-KvEN6O7UDHv29gDBiGHW68W1M6AAJmgmTHWk_oJW2Y6NW8BCVlolQaXuIpzqUgSwIK7tet-i9fc0bW4aZzs6qmmVXsED-TR4gbkLAEMt47CGwX0Gdi4VCN3CyPOOMlRArI18eGr2rh5IDf_ebucCKx23MdigRWNoSVm1bCEHFoxzifmDkCvTAY5-cxoNXAbEF88G1Ke5FRmhgu40RYHommurD6fglsQX9ZVLtYTwClB7kP2IlcYQoeDUTvnDc5__Pn9F8Z6mBo2G15MeIxKdPxoDj45VamzzhkZMbsaYCU"
+};
 
 export class Loop
 {
@@ -11,6 +17,11 @@ export class Loop
         this.pushButton = document.querySelector('.btnNotify');
         this.isSubscribed = false;
         this.swRegistration = null;
+        
+        this.server = "https://loop.goodbytes.be";
+        this.server = "http://loop.goodbytes.local"
+        
+        
         
         this.setup();
     }
@@ -161,6 +172,19 @@ export class Loop
                 document.querySelector('.js-subscription-details');
 
         if (subscription) {
+            
+          // sync the subscription with the server    
+          axios.post(this.server + '/api/v1/subscriptions/sync', {
+            subscription: subscription
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+            
+            
             subscriptionJson.textContent = JSON.stringify(subscription);
             subscriptionDetails.classList.remove('is-invisible');
         } else {
