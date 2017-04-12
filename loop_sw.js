63f365a3c7c9a3a4536962518d3f7970e44be64e
Joris Hens <joris@goodbytes.be>
2017-04-12 14:18:34 +0200

    fix payload

--- loop_sw.js
+++ loop_sw.js
@@ -9,11 +9,13 @@ self.addEventListener('push', function(event) {
     console.log('[Service Worker] Push Received.');
     console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-    const title = 'Push Codelab';
+    var jsonData = event.data.json();
+    
+    const title = jsonData.body;
     const options = {
-        body: 'Yay it works.',
-        icon: 'images/icon.png',
-        badge: 'images/badge.png'
+        body: jsonData.title,
+        icon: jsonData.icon,
+        badge: jsonData.icon
     };
 
     event.waitUntil(self.registration.showNotification(title, options));
